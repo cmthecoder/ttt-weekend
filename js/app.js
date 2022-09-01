ccfd57b7dfae4e9eee9982880b612c54aeef6384
Chris Mems <christopher.mems@.com>
2022-09-01 18:18:52 -0500

    Intialize game state

--- js/app.js
+++ js/app.js
@@ -12,12 +12,21 @@ let board, turn, winner
 const squareEls = document.querySelectorAll('.square')
 
 const messageEl = document.getElementById('message')
-console.log(squareEls)
-console.log(messageEl)
+// console.log(squareEls)
+// console.log(messageEl)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+
+function init(){
+  board = [0,1,2,3,4,5,6,7,8]
+  turn = 1
+  winner === null
+  render()
+}
+
 
