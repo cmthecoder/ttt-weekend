ffaea1303413ca00355e12d71f4b84390a17f615
Chris Mems <christopher.mems@.com>
2022-09-03 17:07:33 -0500

    Render game to the user step 4

--- js/app.js
+++ js/app.js
@@ -22,22 +22,46 @@ const messageEl = document.getElementById('message')
 /*-------------------------------- Functions --------------------------------*/
 init()
 
-function init(){
-  board = [null, null, null, null, null, null, null, null, null]
-  turn = 1
-  winner === null
-  render()
-}
 
 function render(){
   board.forEach((box, idx) => {
     if(box === 1){
       squareEls[idx].textContent = 'X'
+      squareEls[idx].style.color = 'yellow'
+      
+      // style the square depending on the value
     }
     if(box === -1){
       squareEls[idx].textContent = 'O'
+      squareEls[idx].style.color = 'blue'
+      // style the square depending on the value
+    }
+    
+    if(winner === null){
+      if(turn === 1){
+        messageEl.textContent = "Player X's Turn!"
+      } else{
+        messageEl.textContent = "Player O's Turn!"
+      }
+    }  else if(winner === 'T'){
+      messageEl.textContent = "It's a tie!"
+    } else {
+      messageEl.textContent = 'Congradulations Player ' + turn + ' has Won!'
     }
   });
 }
 
-console.log('box')
+function init(){
+  board = [null, null, null, null, null, null, null, null, null]
+  turn = 1
+  winner = null
+  render()
+}
+
+// if(winner === null){
+//   if(box === 1){
+//     messageEl.textContent = "Player O turn"
+//   }
+// } else {
+//   messageEl.textContent ="gg"
+// }
